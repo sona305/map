acc039f92a2792a6f9bec7a8e7b611f6dfb7260a
Swapnil-Sharma <swapnil.sharma7747@gmail.com>
2022-02-23 13:55:43 +0530

    fixed map issue

--- src/App.js
+++ src/App.js
@@ -14,18 +14,16 @@ import "./App.css";
 function Map({uid}) {
   const url = "https://apis.intangles.com/vehicle/getlist?pnum=1&psize=10&lastloc=true&drivers=true&get_account_names=true&only_owner=true&token=amAIZY2JHdAuy-Se7BotfvB3XpUM_O3j_OL4H0SrgQbMZZ5NjYWuIR-hbjI6BHsS&acc_id=643140934522896384";
 
-  var [laty, setLat] = useState(""); 
-  var [longy, setLong] = useState("");
+  const [laty, setLat] = useState(""); 
+  const [longy, setLong] = useState("");
 
   useEffect(()=>{
     axios.get(url).then((res) => { 
       setLat(parseFloat(res.data.v[0].location[1]));
       setLong(parseFloat(res.data.v[0].location[0]));
-    
       })
-
   },[])
-  console.log(laty,longy);
+
   return (
     <>
     {
